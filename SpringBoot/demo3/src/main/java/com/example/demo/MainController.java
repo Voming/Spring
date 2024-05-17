93ef616f2fc69d1f94cd69d97f781f1313324958
Voming <ebomin1234@gmail.com>
2024-05-17 15:47:22 +0900

    jsp localhost
    tyymeleaf를 넣어두면 jsp가 돌아가지 않음

---
+++ SpringBoot/demo3/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+	@GetMapping("/main")
+	public String main(Model model) {
+		model.addAttribute("m1", "m1값");
+		return "aaa";
+	}
+}
